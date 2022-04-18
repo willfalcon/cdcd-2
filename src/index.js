dd57163608c81072fe9b375b268a81269134904c
Will Hawks <willkhawks@gmail.com>
2022-04-18 12:11:06 -0500

    use apollo link with credentials

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 import App from './App';
 
 const container = document.getElementById('root');
 const root = ReactDOMClient.createRoot(container);
 
-const development = process.env.NODE_ENV === 'development';
-const client = new ApolloClient({
+const development = process?.env?.NODE_ENV === 'development';
+const httpLink = new HttpLink({
   uri: development ? 'http://localhost:4000' : 'https://cdcd-server.creativedistillery.com',
+  credentials: 'include',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  // uri: 'https://cdcd-server.creativedistillery.com',
   cache: new InMemoryCache(),
 });
 
